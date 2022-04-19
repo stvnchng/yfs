3e12ee23c65f1d6273f79ae162060032b50b8f7e
stvnchng <sc111@rice.edu>
2022-04-18 23:58:39 -0500

    remove extra definitions in header

--- yfs/iohelpers.h
+++ yfs/iohelpers.h
@@ -1,11 +1,4 @@
 /**
- * definitions
- */
-#define OPEN 0
-#define CREATE 1
-
-
-/**
  * Helper functions for iolib.c
  */
 int compare_filenames(char *f1, char *f2);
@@ -13,6 +6,6 @@ int get_num_blocks(struct inode *inode);
 struct inode *get_inode(short inum);
 int process_path(char *path, int curr_inum, int call_type);
 void add_free_inode(int inum);
-short remove_free_inode();
+short remove_free_inode(short type);
 void add_free_block(int blocknum);
 short remove_free_block();

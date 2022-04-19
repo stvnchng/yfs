c519f70f524e0296757bec9832fcf00d26d6e42f
ooyjian <ouyujian14@gmail.com>
2022-04-18 23:51:31 -0500

    bug fix

--- yfs/iohelpers.h
+++ yfs/iohelpers.h
@@ -1,10 +1,17 @@
 /**
+ * definitions
+ */
+#define OPEN 0
+#define CREATE 1
+
+
+/**
  * Helper functions for iolib.c
  */
 int compare_filenames(char *f1, char *f2);
 int get_num_blocks(struct inode *inode);
 struct inode *get_inode(short inum);
-int process_path(char *path, int curr_inum);
+int process_path(char *path, int curr_inum, int call_type);
 void add_free_inode(int inum);
 short remove_free_inode();
 void add_free_block(int blocknum);

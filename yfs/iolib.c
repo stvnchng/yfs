6ebcbd9ae37a625d3e53b614e2932108c94750d8
stvnchng <sc111@rice.edu>
2022-04-19 22:05:08 -0500

    finish iolib.c

--- yfs/iolib.c
+++ yfs/iolib.c
@@ -17,27 +17,36 @@ int curr_inum = ROOTINODE;
 /**
  *  IOLib helper functions prototypes
  */
+int GetPathLen(char *pathname);
 int SendMessage(int type);
 int SendMessageWithPath(int type, char *pathname);
-// int SendRWMessage(int type, struct opened_file curr_file, );
+int SendMessageRW(int type, struct opened_file curr_file, void* buf, int size);
 
 /*
  *  YFS Message Send Handlers
  */
 int Open(char *pathname)
 {
-    SendMessageWithPath(OPEN, pathname);
+    if (SendMessageWithPath(OPEN, pathname) == ERROR) {
+        printf("Error in send message for Open\n");
+        return ERROR;
+    }
 
     int fd = 0;
     while (fd < MAX_OPEN_FILES) {
         if (opened_files[fd].occupied == 0) {
-            opened_files[fd].inum = curr_inum;
-            opened_files[fd].occupied = 1;
-            opened_files[fd].position = 0;
             break;
         }
-        fd++;
     }
+
+    if (fd == MAX_OPEN_FILES) {
+        printf("Error: fd for %s out of bounds\n", pathname);
+        return ERROR;
+    }
+
+    opened_files[fd].inum = curr_inum;
+    opened_files[fd].occupied = 1;
+    opened_files[fd].position = 0;
     
     return fd;
 }
@@ -56,46 +65,72 @@ int Close(int fd)
 
 int Create(char *pathname)
 {
-    SendMessageWithPath(CREATE, pathname);
+    if (SendMessageWithPath(CREATE, pathname) == ERROR) {
+        printf("Error in send message for Create\n");
+        return ERROR;
+    }
     
     int fd = 0;
     while (fd < MAX_OPEN_FILES) {
-        // find the lowest fd that is not occupied and return it
         if (opened_files[fd].occupied == 0) {
-            opened_files[fd].inum = curr_inum;
-            opened_files[fd].occupied = 1;
-            opened_files[fd].position = 0;
             break;
         }
-        fd++;
     }
 
     if (fd == MAX_OPEN_FILES) {
-        printf("Could not create valid fd\n");
+        printf("Error: fd for %s out of bounds\n", pathname);
         return ERROR;
     }
 
-    return fd;
-}
+    opened_files[fd].inum = curr_inum;
+    opened_files[fd].occupied = 1;
+    opened_files[fd].position = 0;
 
-int Read(int fd, void *buf, int size)
-{
-    (void)fd;
-    (void)buf;
-    (void)size;
     return 0;
 }
 
-int Write(int  fd, void *buf, int size)
+int Read(int fd, void *buf, int size) // TODO might need to read over
 {
+   if (fd < 0 || fd > MAX_OPEN_FILES - 1 || buf == NULL || size < 0) {
+        printf("Bad arguments to Read\n");
+        return ERROR;
+    }
+
     struct opened_file curr_file = opened_files[fd];
-    if (curr_file.occupied == 0 || fd < 0 || fd > MAX_OPEN_FILES - 1 || buf == NULL || size < 0) {
+    if (curr_file.occupied == 0) {
+        printf("Error: Write to a nonexistent file\n");
+        return ERROR;
+    }
+
+    int bytes_read = SendMessageRW(READ, curr_file, buf, size);
+    if (bytes_read == ERROR) {
+        printf("Error in SendRWMessage for Write\n");
+        return ERROR;
+    }
+
+    // increment position of file to be written
+    curr_file.position += bytes_read;
+    return bytes_read;
+}
+
+int Write(int fd, void *buf, int size)
+{
+    if (fd < 0 || fd > MAX_OPEN_FILES - 1 || buf == NULL || size < 0) {
         printf("Bad arguments to Write\n");
         return ERROR;
     }
 
-    int bytes_written = 0;
+    struct opened_file curr_file = opened_files[fd];
+    if (curr_file.occupied == 0) {
+        printf("Error: Write to a nonexistent file\n");
+        return ERROR;
+    }
 
+    int bytes_written = SendMessageRW(WRITE, curr_file, buf, size);
+    if (bytes_written == ERROR) {
+        printf("Error in SendRWMessage for Write\n");
+        return ERROR;
+    }
 
     // increment position of file to be written
     curr_file.position += bytes_written;
@@ -109,14 +144,37 @@ int Seek(int  fd, int offset, int whence)
 
 int Link(char *oldname, char *newname)
 {
-    (void)oldname;
-    (void)newname;
+    if (oldname == NULL || newname == NULL) {
+        printf("Bad arguments to Link\n");
+        return ERROR;
+    }
+    if (GetPathLen(oldname) == ERROR || GetPathLen(newname) == ERROR) {
+        printf("Invalid arg length to Link\n");
+        return ERROR;
+    }
+
+    struct msg *msg = malloc(sizeof(struct msg));
+    msg->type = LINK;
+    msg->data1 = curr_inum; //store inum
+    msg->data2 = GetPathLen(oldname); //store len for server CopyFrom operation 
+    msg->data3 = GetPathLen(newname); //store len for server CopyFrom operation 
+    msg->ptr1 = oldname;
+    msg->ptr2 = newname;
+    if (Send((void *) msg, -FILE_SERVER) == ERROR) {
+        free(msg);
+        printf("Error during Send for STAT\n");
+        return ERROR;
+    }
+    free(msg);
     return 0;
 }
 
 int Unlink(char *pathname)
 {   
-    (void)pathname;
+    if (SendMessageWithPath(UNLINK, pathname) == ERROR) {
+        printf("Error in send message for Unlink\n");
+        return ERROR;
+    }
     return 0;
 }
 
@@ -125,26 +183,50 @@ int Unlink(char *pathname)
 
 int MkDir(char *pathname)
 {
-    (void)pathname;
+    if (SendMessageWithPath(MKDIR, pathname) == ERROR) {
+        printf("Error in send message for Unlink\n");
+        return ERROR;
+    }
     return 0;
 }
 
 int RmDir(char *pathname)
 {
-    (void)pathname;
+    if (SendMessageWithPath(RMDIR, pathname) == ERROR) {
+        printf("Error in send message for Unlink\n");
+        return ERROR;
+    }
     return 0;
 }
 
 int ChDir(char *pathname)
 {
-    (void)pathname;
+    if (SendMessageWithPath(CHDIR, pathname) == ERROR) {
+        printf("Error in send message for Unlink\n");
+        return ERROR;
+    }
     return 0;
 }
 
 int Stat(char *pathname, struct Stat *statbuf)
-{
-    (void)pathname;
-    (void)statbuf;
+{    
+    if (pathname == NULL || statbuf == NULL || GetPathLen(pathname)== ERROR ) {
+        printf("Path length not valid in Stat\n");
+        return ERROR;
+    }
+    
+    struct msg *msg = malloc(sizeof(struct msg));
+    msg->type = STAT;
+    msg->data1 = curr_inum; //store inum
+    msg->data2 = GetPathLen(pathname); //store len for server CopyFrom operation 
+    msg->ptr1 = pathname;
+    msg->ptr2 = statbuf;
+    if (Send((void *) msg, -FILE_SERVER) == ERROR) {
+        free(msg);
+        printf("Error during Send for STAT\n");
+        return ERROR;
+    }
+    free(msg);
     return 0;
 }
 
@@ -175,27 +257,15 @@ int SendMessage(int type)
 /* Helper for requests with pathname param */
 int SendMessageWithPath(int type, char *pathname)
 {
-    if (strlen(pathname) > MAXPATHNAMELEN) {
-        printf("Error: pathname %s longer than MAXPATHNAMELEN\n", pathname);
-        return ERROR;
-    }
-
-    int i = 0;
-    while (i < MAX_OPEN_FILES) {
-        if (opened_files[i].occupied == 0) {
-            break;
-        }
-    }
-
-    if (i == MAX_OPEN_FILES) {
-        printf("Error: fd for %s out of bounds, index %d\n", pathname, i);
+    if (GetPathLen(pathname) == ERROR) {
+        printf("Error: pathname has invalid length\n");
         return ERROR;
     }
     
     struct msg *msg = malloc(sizeof(struct msg));
     msg->type = type;
     msg->data1 = curr_inum; // store inode_num
-    msg->data2 = strlen(pathname);
+    msg->data2 = GetPathLen(pathname); // store len for server CopyFrom operation
     msg->ptr1 = pathname;
 
     if (Send((void *) msg, -FILE_SERVER) == ERROR) {
@@ -204,15 +274,39 @@ int SendMessageWithPath(int type, char *pathname)
         return ERROR;
     }
     free(msg);
-    return i; // return fd
+    return 0;
 }
 
-// int SendRWMessage(int type, struct opened_file curr_file, void* buf, int size)
-// {
-//     int inum = curr_file.inum;
-//     int rw_pos = curr_file.position;
+int SendMessageRW(int type, struct opened_file curr_file, void* buf, int size)
+{
+    if (buf == NULL || size < 0) {
+        printf("Invalid args to SendMessageRW\n");
+        return ERROR;
+    }
 
+     struct msg *msg = malloc(sizeof(struct msg));
+    msg->type = type;
+    msg->data1 = curr_file.inum; // store inode_num
+    msg->data2 = curr_file.position; // store position in curr file
+    msg->data3 = size; // store len for server CopyFrom operation
+    msg->ptr2 = buf;
 
+    if (Send((void *) msg, -FILE_SERVER) == ERROR) {
+        free(msg);
+        printf("Error during Send for type %d\n", msg->type);
+        return ERROR;
+    }
+    free(msg);
+    return 0;
+}
 
-//     return 0;
-// }
+int GetPathLen(char *pathname)
+{
+    int len = 0;
+    while (len < MAXPATHNAMELEN && pathname[len] != '\0') {
+        len++;
+    }
+    if (len == MAXPATHNAMELEN || len == 0) return ERROR;
+    return len;
+}
+
